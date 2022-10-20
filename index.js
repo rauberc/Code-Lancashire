ef7f5dfd2979317c820b1e94c08a1f56f8c51fa7
Cristine <rauberoliveira@gmail.com>
2022-10-20 16:21:22 +0000

    20 Oct 2022, 17:21

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const mainsec = document.querySelector('.mainsec')
 
 const link = document.createElement('a')
 
-link.classList.add('link underline')
+link.classList.add('link')
 
 console.log(mainsec)
